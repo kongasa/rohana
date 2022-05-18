4c248a6396f2789d91a1024e84db6a6cf921056d
kongasa <kongddavid@gmail.com>
2022-05-19 01:22:40 +0800

    LF and lint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    // 'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  // plugins: ['@typescript-eslint'],
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'linebreak-style': ['error', 'unix'],
+  },
+};

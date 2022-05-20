9124332c9b36dcb80476de25d0d16c4e3b338989
kongasa <kongddavid@gmail.com>
2022-05-20 12:24:16 +0800

    inter

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,15 @@ import path from 'path';
 const createWindow = () => {
   Menu.setApplicationMenu(null);
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 900,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
 
   win.loadFile(path.join(__dirname, './html/index.html'));
+  win.webContents.openDevTools();
 };
 
 app.whenReady().then(() => {
--- src/preload.ts
+++ src/preload.ts
@@ -1,12 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector: string, text: string | undefined) => {
-    const element = document.getElementById(selector);
-    if (element) {
-      element.innerText = text ?? '';
-    }
-  };
-
-  for (const dependency of ['chrome', 'node', 'electron']) {
-    replaceText(`${dependency}-version`, process.versions[dependency]);
-  }
+  const searchInput = document.getElementById(
+    'search-input'
+  ) as HTMLInputElement | null;
+  const searchButton = document.getElementById(
+    'search-button'
+  ) as HTMLButtonElement | null;
+  const fileList = document.getElementById(
+    'file-list'
+  ) as HTMLParagraphElement | null;
+  searchButton?.addEventListener('click', () => {
+    const path = searchInput?.value ?? '';
+    if (fileList) fileList.innerText = path;
+  });
 });
